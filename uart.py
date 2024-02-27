610c35aa67355fadea0fae48e5150db76193b0d0
Minh Trí <minhtriinh123@gmail.com>
2024-02-27 14:40:07 +0700

    update uart file

--- uart.py
+++ uart.py
@@ -12,7 +12,7 @@ def getPort():
             splitPort = strPort.split(" ")
             commPort = (splitPort[0])
     # return commPort
-    return "/dev/ttys006"
+    return "/dev/ttys005"
 
 
 # if getPort() != "None":
@@ -46,3 +46,6 @@ def readSerial(client):
                 mess = ""
             else:
                 mess = mess[end+1:]
+
+def writeData(data):
+    ser.write(str(data).encode('utf-8'))
